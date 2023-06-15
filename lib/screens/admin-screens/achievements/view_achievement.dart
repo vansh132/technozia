e9a133aa38d85c11aed1d36c5782be44390268e9
vansh132 <shah.vansh132@gmail.com>
2023-06-15 16:55:00 +0530

    view achievement screen ui finished

--- lib/screens/admin-screens/achievements/view_achievement.dart
+++ lib/screens/admin-screens/achievements/view_achievement.dart
@@ -1,3 +1,4 @@
+import 'package:carousel_slider/carousel_slider.dart';
 import 'package:flutter/material.dart';
 import 'package:technozia/models/achievement.dart';
 import 'package:technozia/services/auth_services.dart';
@@ -28,19 +29,56 @@ class _ViewAchievementScreenState extends State<ViewAchievementScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(),
+        appBar: AppBar(
+          title: const Text("Achievements"),
+          centerTitle: true,
+        ),
         body: achievements == null
-            ? const CircularProgressIndicator()
-            : Container(
-                width: double.infinity,
-                padding: const EdgeInsets.all(16),
-                color: Colors.yellow,
-                height: MediaQuery.of(context).size.height,
-                child: ListView.builder(
-                  itemBuilder: (context, index) {
-                    return achievementItem(achievements![index]);
-                  },
-                  itemCount: achievements?.length,
+            ? const Center(child: CircularProgressIndicator())
+            : SingleChildScrollView(
+                child: Column(
+                  children: [
+                    Container(
+                      padding: const EdgeInsets.all(8),
+                      width: double.infinity,
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          const Text(
+                            "Summary: ",
+                            textAlign: TextAlign.start,
+                            style: TextStyle(
+                              color: Colors.black,
+                              fontSize: 24,
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
+                          Text(
+                            "Total Achievements: ${achievements!.length.toString()} ",
+                            textAlign: TextAlign.start,
+                            style: const TextStyle(
+                              color: Colors.black,
+                              fontSize: 20,
+                              fontWeight: FontWeight.w500,
+                            ),
+                          ),
+                          const Divider(),
+                        ],
+                      ),
+                    ),
+                    Container(
+                      width: double.infinity,
+                      padding: const EdgeInsets.only(top: 16, bottom: 16),
+                      // color: Colors.yellow,
+                      height: MediaQuery.of(context).size.height,
+                      child: ListView.builder(
+                        itemBuilder: (context, index) {
+                          return achievementItem(achievements![index]);
+                        },
+                        itemCount: achievements?.length,
+                      ),
+                    ),
+                  ],
                 ),
               ));
   }
@@ -48,10 +86,10 @@ class _ViewAchievementScreenState extends State<ViewAchievementScreen> {
   Widget achievementItem(Achievement achievement) {
     return Container(
       width: double.infinity,
-      margin: EdgeInsets.only(bottom: 13),
-      height: 550,
-      padding: EdgeInsets.all(16),
-      color: Colors.black,
+      margin: const EdgeInsets.only(bottom: 13),
+      // height: 550,
+      padding: const EdgeInsets.all(16),
+      // color: Colors.green,
       child: Column(
         children: [
           Column(
@@ -64,14 +102,110 @@ class _ViewAchievementScreenState extends State<ViewAchievementScreen> {
                     Text(
                       achievement.title,
                       textAlign: TextAlign.start,
+                      style: const TextStyle(
+                        fontSize: 24,
+                        fontWeight: FontWeight.w800,
+                        color: Colors.black,
+                      ),
                     ),
                     Text(
                       achievement.category,
+                      style: const TextStyle(
+                        fontSize: 16,
+                        fontWeight: FontWeight.w400,
+                        color: Colors.black,
+                      ),
                     ),
                   ],
                 ),
-              )
+              ),
+              const SizedBox(
+                height: 20,
+              ),
+              CarouselSlider(
+                options: CarouselOptions(
+                  height: 200,
+                  aspectRatio: 16 / 9,
+                  viewportFraction: 0.8,
+                  initialPage: 0,
+                  enableInfiniteScroll: true,
+                  reverse: false,
+                  autoPlay: true,
+                  autoPlayInterval: const Duration(seconds: 3),
+                  autoPlayAnimationDuration: const Duration(milliseconds: 800),
+                  autoPlayCurve: Curves.fastOutSlowIn,
+                  enlargeCenterPage: true,
+                  onPageChanged: (index, reason) {
+                    // Do something when the page changes
+                  },
+                  scrollDirection: Axis.horizontal,
+                ),
+                items: achievement.images.map((url) {
+                  return Container(
+                    width: double.infinity,
+                    child: Image.network(url, fit: BoxFit.cover),
+                  );
+                }).toList(),
+              ),
+              const SizedBox(
+                height: 20,
+              ),
+              SizedBox(
+                width: double.infinity,
+                child: Text(
+                  achievement.description,
+                  textAlign: TextAlign.start,
+                  style: const TextStyle(
+                    fontSize: 16,
+                    fontWeight: FontWeight.w300,
+                    color: Colors.black,
+                  ),
+                ),
+              ),
+              const SizedBox(
+                height: 10,
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Text(
+                    "#${achievement.tag}",
+                    textAlign: TextAlign.start,
+                    style: const TextStyle(
+                      fontSize: 16,
+                      fontWeight: FontWeight.w500,
+                      color: Colors.black,
+                    ),
+                  ),
+                  RichText(
+                    text: TextSpan(
+                      text: 'No of Participant:  ',
+                      style: const TextStyle(
+                        fontSize: 16,
+                        fontWeight: FontWeight.bold,
+                        color: Colors.black,
+                      ),
+                      children: <TextSpan>[
+                        TextSpan(
+                          text: achievement.noOfParticipant.toString(),
+                          style: const TextStyle(
+                            fontSize: 16,
+                            fontWeight: FontWeight.w400,
+                            color: Colors.black,
+                          ),
+                        ),
+                      ],
+                    ),
+                  )
+                ],
+              ),
             ],
+          ),
+          const SizedBox(
+            height: 8,
+          ),
+          const Divider(
+            color: Colors.black,
           ),
         ],
       ),
