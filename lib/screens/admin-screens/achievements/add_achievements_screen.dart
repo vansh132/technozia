26f38b9d2286d9423c3b6e30c506d7364146b7d4
vansh132 <shah.vansh132@gmail.com>
2023-06-15 17:05:28 +0530

    add achievement screen ui finished

--- lib/screens/admin-screens/achievements/add_achievements_screen.dart
+++ lib/screens/admin-screens/achievements/add_achievements_screen.dart
@@ -3,6 +3,7 @@ import 'dart:io';
 import 'package:carousel_slider/carousel_slider.dart';
 import 'package:dotted_border/dotted_border.dart';
 import 'package:flutter/material.dart';
+import 'package:technozia/constants/global_variables.dart';
 import 'package:technozia/constants/utils.dart';
 import 'package:technozia/services/auth_services.dart';
 
@@ -45,7 +46,6 @@ class _AddAchievementScreenState extends State<AddAchievementScreen> {
       tag: _tag.text,
       images: images,
     );
-    print("added achievement..." + _noOfParticipants.text);
   }
 
   void selectImages() async {
@@ -58,132 +58,253 @@ class _AddAchievementScreenState extends State<AddAchievementScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(),
+        appBar: AppBar(
+          title: const Text("Add your Achievements"),
+          centerTitle: true,
+        ),
         body: SingleChildScrollView(
-          child: Form(
-            key: _addAchievementFormKey,
-            child: Column(
-              children: [
-                images.isNotEmpty
-                    ? CarouselSlider(
-                        items: images.map((i) {
-                          return Builder(
-                            builder: (context) => Image.file(
-                              i,
-                              fit: BoxFit.cover,
-                              height: 200,
-                            ),
-                          );
-                        }).toList(),
-                        options: CarouselOptions(
-                          viewportFraction: 1,
-                          height: 200,
-                        ),
-                      )
-                    : GestureDetector(
-                        onTap: selectImages,
-                        child: DottedBorder(
-                          borderType: BorderType.RRect,
-                          radius: const Radius.circular(10),
-                          dashPattern: const [10, 4],
-                          strokeCap: StrokeCap.round,
-                          child: Container(
-                            width: double.infinity,
-                            height: 150,
-                            decoration: BoxDecoration(
-                              borderRadius: BorderRadius.circular(10),
-                            ),
-                            child: Column(
-                              mainAxisAlignment: MainAxisAlignment.center,
-                              children: [
-                                const Icon(
-                                  Icons.folder_open,
-                                  size: 40,
-                                ),
-                                const SizedBox(height: 15),
-                                Text(
-                                  'Select Product Images',
-                                  style: TextStyle(
-                                    fontSize: 15,
-                                    color: Colors.grey.shade400,
+          child: Container(
+            padding: const EdgeInsets.all(16),
+            child: Form(
+              key: _addAchievementFormKey,
+              child: Column(
+                children: [
+                  images.isNotEmpty
+                      ? CarouselSlider(
+                          items: images.map((i) {
+                            return Builder(
+                              builder: (context) => Image.file(
+                                i,
+                                fit: BoxFit.cover,
+                                height: 200,
+                              ),
+                            );
+                          }).toList(),
+                          options: CarouselOptions(
+                            viewportFraction: 1,
+                            height: 200,
+                          ),
+                        )
+                      : GestureDetector(
+                          onTap: selectImages,
+                          child: DottedBorder(
+                            borderType: BorderType.RRect,
+                            radius: const Radius.circular(10),
+                            dashPattern: const [10, 4],
+                            strokeCap: StrokeCap.round,
+                            child: Container(
+                              width: double.infinity,
+                              height: 150,
+                              decoration: BoxDecoration(
+                                borderRadius: BorderRadius.circular(10),
+                              ),
+                              child: Column(
+                                mainAxisAlignment: MainAxisAlignment.center,
+                                children: [
+                                  const Icon(
+                                    Icons.folder_open,
+                                    size: 40,
                                   ),
-                                ),
-                              ],
+                                  const SizedBox(height: 15),
+                                  Text(
+                                    'Select Achievement Images',
+                                    style: TextStyle(
+                                      fontSize: 15,
+                                      color: Colors.grey.shade400,
+                                    ),
+                                  ),
+                                ],
+                              ),
                             ),
                           ),
                         ),
+                  const SizedBox(
+                    height: 12,
+                  ),
+                  TextFormField(
+                    controller: _title,
+                    decoration: InputDecoration(
+                      labelText: 'Title',
+                      labelStyle: const TextStyle(
+                        color: Colors.grey,
+                        fontSize: 16.0,
                       ),
-                TextFormField(
-                  controller: _title,
-                  decoration: const InputDecoration(
-                    hintText: 'title',
+                      enabledBorder: OutlineInputBorder(
+                        borderSide: const BorderSide(color: Colors.grey),
+                        borderRadius: BorderRadius.circular(10.0),
+                      ),
+                      focusedBorder: OutlineInputBorder(
+                        borderSide:
+                            BorderSide(color: GlobalVariables.primaryColor),
+                        borderRadius: BorderRadius.circular(10.0),
+                      ),
+                      filled: true,
+                      fillColor: Colors.grey.withOpacity(0.1),
+                      contentPadding: const EdgeInsets.symmetric(
+                          vertical: 16.0, horizontal: 16.0),
+                    ),
+                    validator: (val) {
+                      if (val == null || val.isEmpty) {
+                        return 'Enter title';
+                      }
+                      return null;
+                    },
                   ),
-                  validator: (val) {
-                    if (val == null || val.isEmpty) {
-                      return 'Enter title';
-                    }
-                    return null;
-                  },
-                ),
-                TextFormField(
-                  controller: _category,
-                  decoration: const InputDecoration(
-                    hintText: 'category',
+                  const SizedBox(
+                    height: 12,
                   ),
-                  validator: (val) {
-                    if (val == null || val.isEmpty) {
-                      return 'Enter category';
-                    }
-                    return null;
-                  },
-                ),
-                TextFormField(
-                  controller: _description,
-                  decoration: const InputDecoration(
-                    hintText: 'description',
+                  TextFormField(
+                    controller: _category,
+                    decoration: InputDecoration(
+                      labelText: 'Category',
+                      labelStyle: const TextStyle(
+                        color: Colors.grey,
+                        fontSize: 16.0,
+                      ),
+                      enabledBorder: OutlineInputBorder(
+                        borderSide: const BorderSide(color: Colors.grey),
+                        borderRadius: BorderRadius.circular(10.0),
+                      ),
+                      focusedBorder: OutlineInputBorder(
+                        borderSide:
+                            BorderSide(color: GlobalVariables.primaryColor),
+                        borderRadius: BorderRadius.circular(10.0),
+                      ),
+                      filled: true,
+                      fillColor: Colors.grey.withOpacity(0.1),
+                      contentPadding: const EdgeInsets.symmetric(
+                          vertical: 16.0, horizontal: 16.0),
+                    ),
+                    validator: (val) {
+                      if (val == null || val.isEmpty) {
+                        return 'Enter category';
+                      }
+                      return null;
+                    },
                   ),
-                  validator: (val) {
-                    if (val == null || val.isEmpty) {
-                      return 'Enter Description';
-                    }
-                    return null;
-                  },
-                ),
-                TextFormField(
-                  controller: _noOfParticipants,
-                  decoration: const InputDecoration(
-                    hintText: 'No of participant',
+                  const SizedBox(
+                    height: 12,
                   ),
-                  validator: (val) {
-                    if (val == null || val.isEmpty) {
-                      return 'Enter valid participants';
-                    }
-                    return null;
-                  },
-                ),
-                TextFormField(
-                  controller: _tag,
-                  decoration: const InputDecoration(
-                    hintText: 'Tag',
+                  TextFormField(
+                    controller: _description,
+                    decoration: InputDecoration(
+                      labelText: 'Description',
+                      labelStyle: const TextStyle(
+                        color: Colors.grey,
+                        fontSize: 16.0,
+                      ),
+                      enabledBorder: OutlineInputBorder(
+                        borderSide: const BorderSide(color: Colors.grey),
+                        borderRadius: BorderRadius.circular(10.0),
+                      ),
+                      focusedBorder: OutlineInputBorder(
+                        borderSide:
+                            BorderSide(color: GlobalVariables.primaryColor),
+                        borderRadius: BorderRadius.circular(10.0),
+                      ),
+                      filled: true,
+                      fillColor: Colors.grey.withOpacity(0.1),
+                      contentPadding: const EdgeInsets.symmetric(
+                          vertical: 16.0, horizontal: 16.0),
+                    ),
+                    validator: (val) {
+                      if (val == null || val.isEmpty) {
+                        return 'Enter Description';
+                      }
+                      return null;
+                    },
                   ),
-                  validator: (val) {
-                    if (val == null || val.isEmpty) {
-                      return 'Enter tag';
-                    }
-                    return null;
-                  },
-                ),
-                ElevatedButton(
-                  onPressed: () {
-                    if (_addAchievementFormKey.currentState!.validate()) {
-                      onSubmit();
-                    }
-                  },
-                  child: const Text(
-                    "Sign up",
+                  const SizedBox(
+                    height: 12,
                   ),
-                ),
-              ],
+                  TextFormField(
+                    controller: _noOfParticipants,
+                    decoration: InputDecoration(
+                      labelText: 'No of Participants',
+                      labelStyle: const TextStyle(
+                        color: Colors.grey,
+                        fontSize: 16.0,
+                      ),
+                      enabledBorder: OutlineInputBorder(
+                        borderSide: const BorderSide(color: Colors.grey),
+                        borderRadius: BorderRadius.circular(10.0),
+                      ),
+                      focusedBorder: OutlineInputBorder(
+                        borderSide:
+                            BorderSide(color: GlobalVariables.primaryColor),
+                        borderRadius: BorderRadius.circular(10.0),
+                      ),
+                      filled: true,
+                      fillColor: Colors.grey.withOpacity(0.1),
+                      contentPadding: const EdgeInsets.symmetric(
+                          vertical: 16.0, horizontal: 16.0),
+                    ),
+                    validator: (val) {
+                      if (val == null || val.isEmpty) {
+                        return 'Enter valid participants';
+                      }
+                      return null;
+                    },
+                  ),
+                  const SizedBox(
+                    height: 12,
+                  ),
+                  TextFormField(
+                    controller: _tag,
+                    decoration: InputDecoration(
+                      labelText: 'Tag',
+                      labelStyle: const TextStyle(
+                        color: Colors.grey,
+                        fontSize: 16.0,
+                      ),
+                      enabledBorder: OutlineInputBorder(
+                        borderSide: const BorderSide(color: Colors.grey),
+                        borderRadius: BorderRadius.circular(10.0),
+                      ),
+                      focusedBorder: OutlineInputBorder(
+                        borderSide:
+                            BorderSide(color: GlobalVariables.primaryColor),
+                        borderRadius: BorderRadius.circular(10.0),
+                      ),
+                      filled: true,
+                      fillColor: Colors.grey.withOpacity(0.1),
+                      contentPadding: const EdgeInsets.symmetric(
+                          vertical: 16.0, horizontal: 16.0),
+                    ),
+                    validator: (val) {
+                      if (val == null || val.isEmpty) {
+                        return 'Enter tag';
+                      }
+                      return null;
+                    },
+                  ),
+                  const SizedBox(
+                    height: 16,
+                  ),
+                  ElevatedButton(
+                    onPressed: () {
+                      if (_addAchievementFormKey.currentState!.validate()) {
+                        onSubmit();
+                      }
+                    },
+                    style: ElevatedButton.styleFrom(
+                      foregroundColor: Colors.white,
+                      backgroundColor:
+                          GlobalVariables.primaryColor, // Text color
+                      elevation: 8, // Elevation (shadow)
+                      shape: RoundedRectangleBorder(
+                        borderRadius:
+                            BorderRadius.circular(10.0), // Rounded corners
+                      ),
+                      padding: const EdgeInsets.symmetric(
+                          vertical: 16.0, horizontal: 24.0), // Button padding
+                    ),
+                    child: const Text(
+                      "Add Achievement",
+                    ),
+                  ),
+                ],
+              ),
             ),
           ),
         ));
